a88a401a9c9f0899bd218f98370b24f4c6972c64
Merlin Carter <merlin+demo@quix.io>
2024-04-01 08:31:45 +0000

    Edited 'main.py'

--- Create embeddings/main.py
+++ Create embeddings/main.py
@@ -1,24 +1,44 @@
+from quixstreams import Application 
+from quixstreams import State, message_key
+from sentence_transformers import SentenceTransformer
 import os
-from quixstreams import Application
+import time
 
-# for local dev, load env vars from a .env file
-from dotenv import load_dotenv
-load_dotenv()
+encoder = SentenceTransformer('all-MiniLM-L6-v2') # Model to create embeddings
 
-app = Application.Quix("transformation-v1", auto_offset_reset="earliest")
+# Define the embedding function
+def create_embeddings(row):
+    text = row['doc_content']
+    embeddings = encoder.encode(text)
+    embedding_list = embeddings.tolist() # Conversion step because SentenceTransformer outputs a numpy Array but Qdrant expects a plain list
+    print(f'Created vector: "{embedding_list}"')
 
-input_topic = app.topic(os.environ["input"])
-output_topic = app.topic(os.environ["output"])
+    return embedding_list
 
-sdf = app.dataframe(input_topic)
+# Define your application and settings
+app = Application.Quix(
+    consumer_group="embeddings-v1",
+    auto_offset_reset="earliest",
+    producer_extra_config={"allow.auto.create.topics": "true"},
+)
 
-# put transformation logic here
-# see docs for what you can do
-# https://quix.io/docs/get-started/quixtour/process-threshold.html
+# Define an input topic with JSON deserializer
+input_topic = app.topic(os.environ['input'], value_deserializer="json")
 
-sdf = sdf.update(lambda row: print(row))
+# Define an output topic with JSON serializer
+output_topic = app.topic(os.environ['output'], value_serializer="json")
 
+# Initialize a streaming dataframe based on the stream of messages from the input topic:
+sdf = app.dataframe(topic=input_topic)
+sdf = sdf.update(lambda val: print(f"Received update: {val}"))
+
+# Trigger the embedding function for any new messages(rows) detected in the filtered SDF
+sdf["embeddings"] = sdf.apply(create_embeddings, stateful=False)
+
+# Update the timestamp column to the current time in nanoseconds
+sdf["Timestamp"] = sdf["Timestamp"].apply(lambda row: time.time_ns())
+
+# Publish the processed SDF to a Kafka topic specified by the output_topic object.
 sdf = sdf.to_topic(output_topic)
 
-if __name__ == "__main__":
-    app.run(sdf)
+app.run(sdf)
