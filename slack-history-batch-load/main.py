c90b892f76d664dbe046df1c87ace7e68c5fa680
Tomas Neubauer <tomas@quix.io>
2024-05-06 13:21:59 +0000

    Edited 'main.py'

--- slack-history-batch-load/main.py
+++ slack-history-batch-load/main.py
@@ -81,7 +81,7 @@ if __name__ == "__main__":
     all_messages = []
 
     for channel in channels:
-        if channel['id'] is not "C01H1LQHCAU":
+        if channel['id'] != "C01H1LQHCAU":
             continue
         channel_id = channel['id']
         channel_name = channel['name']
