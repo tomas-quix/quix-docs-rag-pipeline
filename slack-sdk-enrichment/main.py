3bf59869198ffcd6a301461d5a1ac14e701a8a9b
Tomas Neubauer <tomas@quix.io>
2024-05-06 15:52:42 +0000

    Edited 'main.py'

--- slack-sdk-enrichment/main.py
+++ slack-sdk-enrichment/main.py
@@ -101,7 +101,7 @@ sdf = sdf.update(download_files)
 
 sdf = sdf.update(lambda row: print(row))
 
-#sdf = sdf.to_topic(output_topic)
+sdf = sdf.to_topic(output_topic)
 
 if __name__ == "__main__":
     app.run(sdf)
