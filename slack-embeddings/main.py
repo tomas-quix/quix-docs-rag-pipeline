00a1c8fcb49db598d189a2b52e141bb122525a6f
Tomas Neubauer <tomas@quix.ai>
2024-08-05 18:25:16 +0200

    Embeddings improved

--- slack-embeddings/main.py
+++ slack-embeddings/main.py
@@ -1,5 +1,4 @@
-from quixstreams import Application 
-from quixstreams import State, message_key
+from quixstreams import Application, message_key
 from sentence_transformers import SentenceTransformer
 import os
 import time
@@ -15,43 +14,62 @@ encoder = SentenceTransformer('all-MiniLM-L6-v2') # Model to create embeddings
 # Define a namespace (can be any valid UUID)
 namespace = uuid.UUID('12345678-1234-5678-1234-567812345678')
 
+def split_string_with_overlap(s, chunk_size, overlap):
+    if chunk_size <= overlap:
+        raise ValueError("Chunk size must be greater than overlap.")
+    
+    chunks = []
+    start = 0
+    while start < len(s):
+        end = start + chunk_size
+        chunks.append(s[start:end])
+        start += (chunk_size - overlap)
+    
+    return chunks
+
 # Define the embedding function
 def create_embeddings(row):
-    text = f"At {datetime.datetime.fromtimestamp(row['event_ts'])}"
-    text += f" in Slack channel {row['channel']}"
-    text += f" user {row['user']} posted: {row['text']}"
+    text = f"{row['text']}"
 
     for file in row['files']:
         text += "\n" + file
     
     for reply in row['replies']:
-        text += f"\nAt {datetime.datetime.fromtimestamp(reply['event_ts'])}"
-        text += f" user {reply['user']} replied to that with text: {reply['text']}"
+        text += f"{reply['text']}"
 
         for file in reply['files']:
             text += "\n" + file
+            
+    text_chunks = split_string_with_overlap(text, 4000, 200)
+    
+    result_chunks = []
         
-   
+    for text_chunk in text_chunks:
         
-    embeddings = encoder.encode(text)
-    embedding_list = embeddings.tolist() # Conversion step because SentenceTransformer outputs a numpy Array but Qdrant expects a plain list
-    print(f'Created vector: "{embedding_list}"')
+        embeddings = encoder.encode(text_chunk)
+        embedding_list = embeddings.tolist() # Conversion step because SentenceTransformer outputs a numpy Array but Qdrant expects a plain list
+        print(f'Created vector: "{embedding_list}"')
 
-    id = f"{bytes.decode(message_key())}"
+        id = f"{bytes.decode(message_key())}"
 
-    return {
-        'page_content': text,
-        'embeddings': embedding_list,
-        'doc_id': id,
-        'metadata': {
-            'title': row['text'],
-            'id': id,
-            'uuid': str(uuid.uuid5(namespace, id)),
-        }
-    }
+        result_chunks.append({
+            'page_content': text_chunk,
+            'embeddings': embedding_list,
+            'doc_id': id,
+            'metadata': {
+                'title': row['text'],
+                'id': id,
+                'uuid': str(uuid.uuid5(namespace, id)),
+                'timestamp': str(datetime.datetime.fromtimestamp(row['event_ts'])),
+                'channel': row['channel'],
+                'author': row['user']
+            }
+        })
+        
+    return result_chunks
 
 # Define your application and settings
-app = Application(consumer_group="slack-embeddings-v1.10",auto_offset_reset="earliest")
+app = Application(consumer_group="slack-embeddings-v1.12",auto_offset_reset="earliest")
 
 # Define an input topic with JSON deserializer
 input_topic = app.topic(os.environ['input'], value_deserializer="json")
@@ -67,7 +85,7 @@ sdf = sdf.update(lambda val: print(f"Received update: {val}"))
 sdf = sdf[sdf.contains("event_ts")]
 
 # Trigger the embedding function for any new messages(rows) detected in the filtered SDF
-sdf = sdf.apply(create_embeddings)
+sdf = sdf.apply(create_embeddings, expand=True)
 
 # Update the timestamp column to the current time in nanosecondsc
 sdf["Timestamp"] = sdf.apply(lambda row: time.time_ns())
