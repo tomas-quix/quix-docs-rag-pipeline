df3d92d44dc3807cc5efb10abb611277931d8b0e
Tomas Neubauer <tomas@quix.io>
2024-08-12 14:23:53 +0000

    Edited 'main.py'

--- qdrant-sink/main.py
+++ qdrant-sink/main.py
@@ -65,8 +65,8 @@ offsetlimit = 125
 #         app.stop()
 
 app = Application.Quix(
-    consumer_group="qdrant-ingestion-v6.9",
-    auto_offset_reset="latest",
+    consumer_group="qdrant-ingestion-v6.10",
+    auto_offset_reset="earliest",
 )
 
 # Define an input topic with JSON deserializer
