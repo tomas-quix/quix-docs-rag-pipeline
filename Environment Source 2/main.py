f03f91fc87c9db04b21a39aa09fc6c74d4596ffb
Tomas Neubauer <tomas@quix.io>
2024-09-18 09:41:25 +0000

    Edited 'main.py'

--- Environment Source 2/main.py
+++ Environment Source 2/main.py
@@ -5,13 +5,11 @@ from quixstreams.sources.kafka import QuixEnvironmentSource
 from dotenv import load_dotenv
 
 def main():
-    app = Application(loglevel=5, )
     
     # Load environment variables from .env file for local development
     load_dotenv()
     
     # Setup output topic
-    output_topic = app.topic(os.environ["topic"])
 
     # Get necessary environment variables for Quix input topic
     source_workspace_id = os.environ["source_workspace_id"]
@@ -20,6 +18,9 @@ def main():
     # Optional environment variables
     consumer_group = os.environ.get("consumer_group", "quix_environment_source")
     auto_offset_reset = os.environ.get("auto_offset_reset",    "earliest")
+
+    app = Application(loglevel=5, consumer_group=consumer_group, auto_offset_reset=auto_offset_reset)
+    output_topic = app.topic(os.environ["topic"])
 
     # Setup input topic
     input_topic = QuixEnvironmentSource(
